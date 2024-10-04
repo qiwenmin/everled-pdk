46e75a51aa6e6a81edd649016b75ec61a72c7ab4
Qi Wenmin <qiwenmin@gmail.com>
2024-10-04 14:31:57 +0800

    refactor: Rewriting in assembly code.
    The code size was reduced by 5 words.

--- everled.c
+++ everled.c
@@ -4,6 +4,8 @@
 
 #define PULSE_PIN 7
 
+#define USE_ASSEMBLY 0
+
 unsigned char __sdcc_external_startup(void) {
     EASY_PDK_INIT_SYSCLOCK_1MHZ();
     EASY_PDK_CALIBRATE_IHRC(1000000, 3000);
@@ -11,6 +13,7 @@ unsigned char __sdcc_external_startup(void) {
 }
 
 void main(void) {
+#if !(USE_ASSEMBLY)
     PADIER = 0; // disable pins as wakeup source
     PBDIER = 0; // Also port B needs to be disabled even if it is not connected
                 // to the outside of the package. touching the package can introduce
@@ -45,4 +48,54 @@ void main(void) {
         // Turn on IHRC again
         EASY_PDK_INIT_SYSCLOCK_1MHZ();
     }
+#else // USE_ASSEMBLY
+    // Below is the optimized code in assembly
+    __asm
+        mov a, #0
+        mov.io __padier, a  // disable pins as wakeup source
+        mov.io __pbdier, a  // Also port B needs to be disabled even if it is not connected
+                            // to the outside of the package. touching the package can introduce
+                            // glitches and wake up the device
+        mov.io __inten, a   // Make sure all interrupts are disabled
+        mov.io __intrq, a
+
+        mov.io __paph, a    // Disable all pull up resistors
+        mov.io __pa, a      // All pins output low
+
+        mov a, #0xFF        // Set all pins output
+        mov.io __pac, a
+
+        mov a, #MISC_FAST_WAKEUP_ENABLE // Enable faster wakeup (45 clocks instead of 3000)
+        mov.io __misc, a
+
+        // Pulse frequency: ~50KHz / 4 / 256 = ~49Hz
+        mov a, #(T16_CLK_ILRC | T16_CLK_DIV4 | T16_INTSRC_8BIT)
+        mov.io __t16m, a
+
+        clear p
+        stt16 p
+
+    loop:
+        // Pulse for about 3uS
+        set1.io __pa, #PULSE_PIN
+        nop
+        nop
+        set0.io __pa, #PULSE_PIN
+
+        // Turn off IHRC to save power
+        mov a, #(CLKMD_ENABLE_ILRC | CLKMD_ENABLE_IHRC | CLKMD_ILRC)
+        mov.io __clkmd, a
+        set0.io __clkmd, #4
+
+        // Wait for the next pulse
+        stopexe
+        nop
+
+        // Turn on IHRC again
+        mov a, #(CLKMD_ENABLE_ILRC | CLKMD_ENABLE_IHRC | CLKMD_IHRC_DIV16)
+        mov.io __clkmd, a
+
+        goto loop
+    __endasm;
+#endif // USE_ASSEMBLY
 }
